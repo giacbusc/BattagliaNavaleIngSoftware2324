59907e30de59e33eaff8fa27c357b8cd3e464f6f
buscst <145765823+buscst@users.noreply.github.com>
2023-11-17 15:12:53 +0100

    Creazione progetto maven "BattagliaNavaleProject"
    Progetto GroupId: "BattagliaNavaleProject" e ArtifactId: "BattagliaNavale"

---
+++ BattagliaNavale/src/test/java/BattagliaNavaleProject/BattagliaNavale/AppTest.java
@@ -0,0 +1,16 @@
+package test.java.BattagliaNavaleProject.BattagliaNavale;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    
+    public void shouldAnswerWithTrue()
+    {
+      
+    }
+}
