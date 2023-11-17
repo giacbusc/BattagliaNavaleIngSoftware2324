1a05a5a2c00a2ae8239cc4a88109c8943e54c9f4
giessse <g.signori1@studenti.unibg.it>
2023-11-17 17:14:33 +0100

    Creazione Package Server e Client
    creazione SchermataIniziale e MenuPrincipale

---
+++ BattagliaNavale/src/BattagliaNavaleProject/client/BattagliaNavaleClient.java
@@ -0,0 +1,41 @@
+package BattagliaNavaleProject.client;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class BattagliaNavaleClient extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BattagliaNavaleClient frame = new BattagliaNavaleClient();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BattagliaNavaleClient() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
---
+++ BattagliaNavale/src/BattagliaNavaleProject/client/MenuPrincipale.java
@@ -0,0 +1,41 @@
+package BattagliaNavaleProject.client;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+//schermata per selezionare partita,visualizzazione classifica e selezione lingua
+public class MenuPrincipale extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuPrincipale frame = new MenuPrincipale();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuPrincipale() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
