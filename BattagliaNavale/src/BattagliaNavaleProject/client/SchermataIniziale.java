0788c0c3d847aa95e75fa032c44f81a509f0c2d1
buscst <145765823+buscst@users.noreply.github.com>
2023-11-18 11:32:37 +0100

    Creazione GUI schermatainiziale di login/registrazione
    Utilizzo di windowbuilder e altri strumenti

--- BattagliaNavale/src/BattagliaNavaleProject/client/SchermataIniziale.java
+++ BattagliaNavale/src/BattagliaNavaleProject/client/SchermataIniziale.java
@@ -5,10 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JLabel;
+import java.awt.Color;
 //pagina per loggarsi/registrarsi
-public class SchermataIniziale extends JFrame {
+public class SchermataIniziale extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	JButton loginButton = new JButton("LOGIN");
+	JButton registerButton = new JButton("REGISTER");
 
 	/**
 	 * Launch the application.
@@ -29,13 +38,48 @@ public class SchermataIniziale extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public SchermataIniziale() {
+	public SchermataIniziale() 
+	{	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		
+		
+		
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(224, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		loginButton.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		loginButton.addActionListener(this);
+		loginButton.setBounds(144, 82, 166, 70);
+		contentPane.add(loginButton);
+		
+		
+		registerButton.addActionListener(this);
+		registerButton.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		registerButton.setBounds(144, 181, 166, 70);
+		contentPane.add(registerButton);
+		
+		JLabel infoLabel = new JLabel("New player? Start here!");
+		infoLabel.setFont(new Font("Shree Devanagari 714", Font.ITALIC, 11));
+		infoLabel.setBounds(165, 164, 132, 16);
+		contentPane.add(infoLabel);
+		
+		JLabel titleLabel = new JLabel("Welcome in BattleShip game");
+		titleLabel.setFont(new Font("Shree Devanagari 714", Font.PLAIN, 26));
+		titleLabel.setBounds(62, 6, 354, 75);
+		contentPane.add(titleLabel);
 	}
-
+	
+	public void actionPerformed(ActionEvent e) 
+	{
+		if (e.getSource() == loginButton) {
+			//gestisci login
+		} else if (e.getSource() == registerButton) {
+			//gestisci registrazione
+		}
+	}
 }
