535262811ec1bd010c04275d39e5a38c58266b01
giessse <g.signori1@studenti.unibg.it>
2023-11-20 12:34:05 +0100

    modifica connection db
    modifica e prova

--- BattagliaNavale/src/BattagliaNavaleProject/server/ConnectionDb.java
+++ BattagliaNavale/src/BattagliaNavaleProject/server/ConnectionDb.java
@@ -8,20 +8,20 @@ import java.sql.Statement;
 
 public class ConnectionDb 
 {
-	String jdbcURL = "jdbc:h2:tcp://localhost/~/test";
-	String username = "sa";
-	String password = "1234";
 	
 
-	public ConnectionDb()
+	public static void main(String[] args)
 	{
+		String jdbcURL = "jdbc:h2:tcp://localhost/~/test";
+		String username = "sa";
+		String password = "1234";
 		try {
 			Connection connection = DriverManager.getConnection(jdbcURL, username, password);
 			System.out.println("Connected to H2 database");
 			String sql = "INSERT INTO UTENTE VALUES('Martin','Espinosa','Martin90','lemiebimbevittime')";
 			
 			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(sql);
+			boolean resultSet = statement.execute(sql);
 			
 			
 			
