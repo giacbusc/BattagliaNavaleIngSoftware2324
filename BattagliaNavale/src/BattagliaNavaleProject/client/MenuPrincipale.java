9fed47b4257b7736c9dbd4a276bfd856af505328
LucaCiancio <l.ciancio@studenti.unibg.it>
2023-11-20 17:16:35 +0100

    Update MenuPrincipale.java

--- BattagliaNavale/src/BattagliaNavaleProject/client/MenuPrincipale.java
+++ BattagliaNavale/src/BattagliaNavaleProject/client/MenuPrincipale.java
@@ -71,47 +71,54 @@ final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
         backgroundPanel.add(lblNewLabel_1);
         
         JLabel lblNewLabel_2 = new JLabel("BATTAGLIA NAVALE ");
-        lblNewLabel_2.setForeground(new Color(0, 0, 255));
+        lblNewLabel_2.setForeground(new Color(0, 128, 255));
         lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 42));
         lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
         lblNewLabel_2.setBounds(129, 0, 499, 85);
         backgroundPanel.add(lblNewLabel_2);
 	    
 
-	    JButton multiplayerbutton = new JButton("Multiplayer");
-	    multiplayerbutton.setBackground(new Color(0, 0, 255));
-	   // multiplayerbutton.setForeground(new Color(0, 0, 255)); senza questa linea leggiamo la scritta sul bottone
-	    multiplayerbutton.setPreferredSize(new Dimension(170, 50));
-	    multiplayerbutton.setBounds(258, 124, 275, 70);
-	    backgroundPanel.add(multiplayerbutton);
+	    JButton multiplayerButton = new JButton("Multiplayer");
+	    multiplayerButton.setPreferredSize(new Dimension(170, 50));
+	    multiplayerButton.setBackground(new Color(0, 0, 0, 0));
+		multiplayerButton.setOpaque(false);
+	    multiplayerButton.setBounds(258, 124, 275, 70);
+	    backgroundPanel.add(multiplayerButton);
 	    
 	    JButton singleplayer = new JButton("Singleplayer");
 	   
         
 	    singleplayer.setBackground(new Color(0, 0, 255));
-	    //singleplayer.setForeground(new Color(0, 0, 255));
+	    singleplayer.setBackground(new Color(0, 0, 0, 0));
+		singleplayer.setOpaque(false);
 	    singleplayer.setPreferredSize(new Dimension(170, 50));
 	    singleplayer.setBounds(258, 247, 275, 70);
 	    backgroundPanel.add(singleplayer);
 	    
-	    JButton tutorialbutton = new JButton("Tutorial");
-	    //tutorialbutton.setForeground(new Color(0, 0, 255));
-	    tutorialbutton.setBackground(new Color(0, 0, 255));
-	    tutorialbutton.setBounds(258, 379, 275, 70);
-	    tutorialbutton.setPreferredSize(new Dimension(170, 50)); // Set the button size in pixels
-	    backgroundPanel.add(tutorialbutton);
+	    JButton tutorialButton = new JButton("Tutorial");
+	    tutorialButton.setBackground(new Color(0, 0, 0, 0));
+		tutorialButton.setOpaque(false);
+	    //tutorialButton.setBorderPainted(false);
+	    tutorialButton.setBounds(258, 379, 275, 70);
+	    tutorialButton.setPreferredSize(new Dimension(170, 50)); // Set the button size in pixels
+	    backgroundPanel.add(tutorialButton);
 	    
-	    
+	    JPanel radioPanel= new JPanel();
+	    radioPanel.setBounds(650,150,100,100);
+	    radioPanel.setOpaque(false);
 	    
 	    ButtonGroup languageGroup = new ButtonGroup();
 	    JRadioButton itaRadioButton = new JRadioButton("ITA");
+	    itaRadioButton.setOpaque(false);
 	    languageGroup.add(itaRadioButton);
-	    backgroundPanel.add(itaRadioButton);
+	    radioPanel.add(itaRadioButton);
 	    
         JRadioButton engRadioButton = new JRadioButton("ENG");
         languageGroup.add(engRadioButton);
-        backgroundPanel.add(engRadioButton);
+        engRadioButton.setOpaque(false);
+        radioPanel.add(engRadioButton);
         
+        backgroundPanel.add(radioPanel);
         JLabel lblNewLabel_3 = new JLabel("Don't play alone, it's funnier with friends!");
         lblNewLabel_3.setBounds(258, 193, 275, 19);
         lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
