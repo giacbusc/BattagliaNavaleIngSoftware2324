1979768079be5436d36d7e166f8491de498132b6
buscst <145765823+buscst@users.noreply.github.com>
2023-11-21 10:07:23 +0100

    Connessione al db da remoto abilitata

--- BattagliaNavale/src/BattagliaNavaleProject/server/ConnectionDb.java
+++ BattagliaNavale/src/BattagliaNavaleProject/server/ConnectionDb.java
@@ -2,15 +2,14 @@ package BattagliaNavaleProject.server;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import org.h2.tools.Server;
+
 
 public class ConnectionDb 
 {
-	private String jdbcURL = "jdbc:h2:tcp://localhost/~/test";
+	private String jdbcURL = "jdbc:h2:tcp://172.16.130.81:9092/~/test";
 	private String username = "sa";
 	private String password = "1234";
 	public ConnectionDb()
@@ -24,11 +23,13 @@ public class ConnectionDb
 			e.printStackTrace();
 		}
 	}
+	
 	public Connection getConnection() throws SQLException
-	 {
-	  Connection connection = DriverManager.getConnection(jdbcURL, username, password);
-	  return connection;
-	 }
+	{
+		Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+		return connection;
+	}
+	
 	public boolean insertQuery(String sql) throws SQLException
 	{
 		Connection connection = DriverManager.getConnection(jdbcURL, username, password);
