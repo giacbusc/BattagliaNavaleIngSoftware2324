dbe7f0f29ec567782abfc7888e75d292e69ba841
LucaCiancio <l.ciancio@studenti.unibg.it>
2023-11-21 10:30:05 +0100

    Miglioramento Grafica
    Inserimento immagine effetto dei bottoni, e implementazione effetti al passaggio del mouse sopra i bottoni.

--- BattagliaNavale/src/BattagliaNavaleProject/client/MenuPrincipale.java
+++ BattagliaNavale/src/BattagliaNavaleProject/client/MenuPrincipale.java
@@ -3,14 +3,12 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.awt.event.ActionEvent;
 
 public class MenuPrincipale extends JFrame {
-	private JPanel panel = new JPanel();
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -36,7 +34,7 @@ public class MenuPrincipale extends JFrame {
 	   
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setSize(771, 600);
-final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
+	    final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
 		
 		JPanel backgroundPanel = new JPanel() {
             @Override
@@ -48,6 +46,23 @@ final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
         getContentPane().add(backgroundPanel);
 		backgroundPanel.setLayout(null);
 		
+		final JLabel lblNewLabel_3 = new JLabel("Don't play alone, it's funnier with friends!");
+        lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 10));
+        lblNewLabel_3.setBounds(258, 193, 275, 19);
+        lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+        backgroundPanel.add(lblNewLabel_3);
+        
+        final JLabel lblNewLabel_4 = new JLabel("Ready to play?");
+        lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 10));
+        lblNewLabel_4.setBounds(258, 315, 275, 19);
+        lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+        backgroundPanel.add(lblNewLabel_4);
+        
+        final JLabel lblNewLabel_5 = new JLabel("Learn how to play with this tutorial");
+        lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 10));
+        lblNewLabel_5.setBounds(258, 447, 275, 19);
+        lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
+        backgroundPanel.add(lblNewLabel_5);
 		
         ImageIcon icon = new ImageIcon("../docs/resources/Logo.jpeg");
         Image image = icon.getImage();
@@ -55,6 +70,13 @@ final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
 
         // scaled icon
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        
+        final JLabel multiplayerTextLabel= new JLabel("Multiplayer");
+        multiplayerTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+        multiplayerTextLabel.setBounds(324,142,142,30);
+        backgroundPanel.add(multiplayerTextLabel);
+       
         JLabel logoLabel = new JLabel(scaledIcon);
         
         
@@ -77,18 +99,84 @@ final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
         lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
         lblNewLabel_2.setBounds(129, 0, 499, 85);
         backgroundPanel.add(lblNewLabel_2);
+       
+        final ImageIcon hoverIcon = new ImageIcon("../docs/resources/EffettoBottone.png");
+	    Image buttonImage = hoverIcon.getImage();
+        Image scaledButtonImage = buttonImage.getScaledInstance(70, 70, Image.SCALE_SMOOTH);
+	    final JLabel imageLabel =new JLabel(new ImageIcon(scaledButtonImage));
+	    final JLabel imageLabel2 =new JLabel(new ImageIcon(scaledButtonImage));
+	    final JLabel imageLabel3 =new JLabel(new ImageIcon(scaledButtonImage));
+	    imageLabel.setBounds(258,124,72,70);
+	    imageLabel2.setBounds(258,247,72,70);
+	    imageLabel3.setBounds(258,379,72,70);
+	    imageLabel.setVisible(false);
+	    imageLabel2.setVisible(false);
+	    imageLabel3.setVisible(false);
+	    backgroundPanel.add(imageLabel);
+	    backgroundPanel.add(imageLabel2);
+	    backgroundPanel.add(imageLabel3);
 	    
 
-	    JButton multiplayerButton = new JButton("Multiplayer");
+	    final JButton multiplayerButton = new JButton("");
 	    multiplayerButton.setPreferredSize(new Dimension(170, 50));
 	    multiplayerButton.setBackground(new Color(0, 0, 0, 0));
 		multiplayerButton.setOpaque(false);
 	    multiplayerButton.setBounds(258, 124, 275, 70);
 	    backgroundPanel.add(multiplayerButton);
 	    
-	    JButton singleplayer = new JButton("Singleplayer");
-	   
+	    
+	    multiplayerButton.addMouseListener(new MouseListener()
+	    {
+	    	public void mouseEntered(MouseEvent e)
+	    	{
+	    		imageLabel.setVisible(true);
+	    		multiplayerTextLabel.setForeground(new Color(0, 128, 255));
+	    		multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
+	    		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+	    		multiplayerButton.repaint();
+	    	}
+	    
+	    
+	    public void mouseExited(MouseEvent e)
+	    {
+	    	imageLabel.setVisible(false);
+	    	multiplayerTextLabel.setForeground(new Color(0, 0, 0));
+	    	multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+	    	lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 10));
+            multiplayerButton.repaint();
+             
+	    }
+
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	    });
+	    
+	    final JLabel singleplayerTextLabel= new JLabel("Singleplayer");
+        singleplayerTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+        singleplayerTextLabel.setBounds(324,266,155,30);
+        backgroundPanel.add(singleplayerTextLabel);
         
+	    final JButton singleplayer = new JButton("");
 	    singleplayer.setBackground(new Color(0, 0, 255));
 	    singleplayer.setBackground(new Color(0, 0, 0, 0));
 		singleplayer.setOpaque(false);
@@ -96,7 +184,66 @@ final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
 	    singleplayer.setBounds(258, 247, 275, 70);
 	    backgroundPanel.add(singleplayer);
 	    
-	    JButton tutorialButton = new JButton("Tutorial");
+	    
+	    singleplayer.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    	}
+	    });
+	    
+	    singleplayer.addMouseListener(new MouseListener()
+	    {
+	    	public void mouseEntered(MouseEvent e)
+	    	{
+	    		imageLabel2.setVisible(true);
+	    		singleplayerTextLabel.setForeground(new Color(0, 128, 255));
+	    		singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
+	    		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
+	    		singleplayer.repaint();
+	    	}
+	    
+	    
+	    public void mouseExited(MouseEvent e)
+	    {
+	    	imageLabel2.setVisible(false);
+	    	singleplayerTextLabel.setForeground(new Color(0, 0, 0));
+	    	singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+	    	lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 10));
+            singleplayer.repaint();
+             
+	    }
+
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	    });
+	   
+        
+	    
+	    final JLabel tutorialTextLabel= new JLabel("Tutorial");
+        tutorialTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+        tutorialTextLabel.setBounds(324,395,155,30);
+        backgroundPanel.add(tutorialTextLabel);
+	    
+	    final JButton tutorialButton = new JButton("");
 	    tutorialButton.setBackground(new Color(0, 0, 0, 0));
 		tutorialButton.setOpaque(false);
 	    //tutorialButton.setBorderPainted(false);
@@ -104,8 +251,53 @@ final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
 	    tutorialButton.setPreferredSize(new Dimension(170, 50)); // Set the button size in pixels
 	    backgroundPanel.add(tutorialButton);
 	    
+	    tutorialButton.addMouseListener(new MouseListener()
+	    {
+	    	public void mouseEntered(MouseEvent e)
+	    	{
+	    		imageLabel3.setVisible(true);
+	    		tutorialTextLabel.setForeground(new Color(0, 128, 255));
+	    		tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
+	    		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 12));
+	    		tutorialButton.repaint();
+	    	}
+	    
+	    
+	    public void mouseExited(MouseEvent e)
+	    {
+	    	imageLabel3.setVisible(false);
+	    	tutorialTextLabel.setForeground(new Color(0, 0, 0));
+	    	tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+	    	lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 10));
+            tutorialButton.repaint();
+             
+	    }
+
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	    });
+	    
 	    JPanel radioPanel= new JPanel();
-	    radioPanel.setBounds(650,150,100,100);
+	    radioPanel.setBounds(638,435,100,100);
 	    radioPanel.setOpaque(false);
 	    
 	    ButtonGroup languageGroup = new ButtonGroup();
@@ -120,21 +312,6 @@ final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
         radioPanel.add(engRadioButton);
         
         backgroundPanel.add(radioPanel);
-        JLabel lblNewLabel_3 = new JLabel("Don't play alone, it's funnier with friends!");
-        lblNewLabel_3.setBounds(258, 193, 275, 19);
-        lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-        backgroundPanel.add(lblNewLabel_3);
-        
-        JLabel lblNewLabel_4 = new JLabel("Ready to play?");
-        lblNewLabel_4.setBounds(258, 315, 275, 19);
-        lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-        backgroundPanel.add(lblNewLabel_4);
-        
-        JLabel lblNewLabel_5 = new JLabel("Learn how to play with this tutorial");
-        lblNewLabel_5.setBounds(258, 447, 275, 19);
-        lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
-        backgroundPanel.add(lblNewLabel_5);
-	            
-        
+	                    
 	}
 }
