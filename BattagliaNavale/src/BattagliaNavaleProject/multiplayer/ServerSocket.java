d942d3e1c5235a20440805ac135db2d4268e1d65
giessse <g.signori1@studenti.unibg.it>
2024-01-18 11:42:36 +0100

    creazione serverSocket

---
+++ BattagliaNavale/src/BattagliaNavaleProject/multiplayer/ServerSocket.java
@@ -0,0 +1,83 @@
+package BattagliaNavaleProject.multiplayer;
+import java.util.ArrayList;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class ServerSocket {
+
+    private int clientCount = 0;
+    private static final ArrayList<String> connectedClients = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        ServerSocket server = new ServerSocket();
+        server.startServer();
+    }
+
+    public void startServer() {
+        ZContext context = new ZContext();
+        ZMQ.Socket socketServer = context.createSocket(SocketType.REP);
+        socketServer.bind("tcp://172.16.129.123:5555");
+
+        try {
+            while (clientCount < 2) {
+                clientCount++;
+                
+                
+                String request = socketServer.recvStr(0);
+                String[] authInfo = request.split("\\|");
+                if (authInfo.length != 1) 
+                {
+               		 socketServer.send("ERROR", 0);
+                }    
+                String username = authInfo[0];
+                
+                
+                if (clientCount == 1) {
+              		connectedClients.add(username);
+                    String responseMessage = "WAIT";
+                    socketServer.send(responseMessage.getBytes(), 0);
+                    System.out.println("Inviato: " + responseMessage);
+                    
+                } else {
+                    if (isUsernameUnique(username)) {
+                		connectedClients.add(username);
+                		String responseMessage = "OK";
+                    	socketServer.send(responseMessage.getBytes(), 0);
+                    	System.out.println("Inviato: " + responseMessage);
+                	}
+                	else
+                	{
+                        String responseMessage = "DUPL";
+                        clientCount--;
+                    	socketServer.send(responseMessage.getBytes(), 0);
+                    	System.out.println("Inviato: " + responseMessage);
+                    
+                    }
+
+                
+            }
+         } 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Chiusura del socket e del contesto alla fine del programma
+            socketServer.close();
+            context.close();
+        }
+    }
+    
+    private static boolean isUsernameUnique(String username) 
+    {
+        
+        for (String str : connectedClients) {
+            if (str.equals(username)) {
+                // The string is not unique, as it already exists in the list
+                return false;
+            }
+        }
+        
+     	return true;
+    }
+}
