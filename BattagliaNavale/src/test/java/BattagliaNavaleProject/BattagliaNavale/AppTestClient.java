4515df4dbbed02ffa6451bb49c4ac348aee71714
buscst <145765823+buscst@users.noreply.github.com>
2024-01-26 13:56:52 +0100

    Add main per il test del client
    Co-Authored-By: giessse <92917662+giessse@users.noreply.github.com>

--- BattagliaNavale/src/test/java/BattagliaNavaleProject/BattagliaNavale/AppTestClient.java
+++ BattagliaNavale/src/test/java/BattagliaNavaleProject/BattagliaNavale/AppTestClient.java
@@ -2,7 +2,10 @@ package test.java.BattagliaNavaleProject.BattagliaNavale;
 
 import BattagliaNavaleProject.Control.ConnectionControl;
 
-public class AppTestClient {
-	ConnectionControl c1 = new ConnectionControl("ciancio");
-	
+public class AppTestClient 
+{
+	public static void main(String[] args)
+	{
+		ConnectionControl c1 = new ConnectionControl("ciancio");
+	}
 }
