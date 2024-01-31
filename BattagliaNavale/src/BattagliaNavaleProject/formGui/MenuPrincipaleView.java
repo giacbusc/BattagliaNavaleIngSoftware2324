bdca3028ef0e41116b73cab8dff634d026fbafe5
LucaCiancio <l.ciancio@studenti.unibg.it>
2024-01-31 14:22:03 +0100

    Update MenuPrincipaleView.java

--- BattagliaNavale/src/BattagliaNavaleProject/formGui/MenuPrincipaleView.java
+++ BattagliaNavale/src/BattagliaNavaleProject/formGui/MenuPrincipaleView.java
@@ -16,7 +16,25 @@ import java.awt.event.ActionEvent;
 
 public class MenuPrincipaleView extends JFrame {
 	private JLabel infoPlayerLabel_1;
-	private String userName;
+	private static String userName;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run()
+			{
+				try 
+				{
+					MenuPrincipaleView frame = new MenuPrincipaleView(userName);
+				    frame.setVisible(true);
+			}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
 
 	
 	public MenuPrincipaleView(String username) throws IOException, SQLException 
@@ -24,7 +42,7 @@ public class MenuPrincipaleView extends JFrame {
 		this.userName = username;
 		
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    setSize(771, 600);
+	    setSize(1400, 788);
 	    final ImageIcon sfondo = new ImageIcon("../docs/resources/SfondoTest.jpeg");
 		
 		JPanel backgroundPanel = new JPanel() {
@@ -38,37 +56,37 @@ public class MenuPrincipaleView extends JFrame {
 		backgroundPanel.setLayout(null);
 		
 		final JLabel lblNewLabel_3 = new JLabel("Don't play alone, it's funnier with friends!");
-        lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 10));
-        lblNewLabel_3.setBounds(258, 193, 275, 19);
+        lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 15));
+        lblNewLabel_3.setBounds(515, 233, 425, 40);
         lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
         backgroundPanel.add(lblNewLabel_3);
         
         final JLabel lblNewLabel_4 = new JLabel("Ready to play?");
-        lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 10));
-        lblNewLabel_4.setBounds(258, 315, 275, 19);
+        lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 15));
+        lblNewLabel_4.setBounds(515, 400, 425, 40);
         lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
         backgroundPanel.add(lblNewLabel_4);
         
         final JLabel lblNewLabel_5 = new JLabel("Learn how to play with this tutorial");
-        lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 10));
-        lblNewLabel_5.setBounds(258, 447, 275, 19);
+        lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 15));
+        lblNewLabel_5.setBounds(515, 560, 425, 40);
         lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
         backgroundPanel.add(lblNewLabel_5);
 		
         ImageIcon icon = new ImageIcon("../docs/resources/Logo.png");
         Image image = icon.getImage();
-        Image scaledImage = image.getScaledInstance(80, 80, Image.SCALE_SMOOTH);
+        Image scaledImage = image.getScaledInstance(160, 160, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
         
         final JLabel multiplayerTextLabel= new JLabel("Multiplayer");
         multiplayerTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-        multiplayerTextLabel.setBounds(324,142,142,30);
+        multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 34));
+        multiplayerTextLabel.setBounds(590,160,282,70);
         backgroundPanel.add(multiplayerTextLabel);
        
         JLabel logoLabel = new JLabel(scaledIcon);
-        logoLabel.setPreferredSize(new Dimension(80,80));
-        logoLabel.setBounds(0, 0, 119, 85);
+        logoLabel.setPreferredSize(new Dimension(200,200));
+        logoLabel.setBounds(0, 0, 200, 200);
         logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
         backgroundPanel.add(logoLabel);
         
@@ -83,20 +101,20 @@ public class MenuPrincipaleView extends JFrame {
        
         JLabel lblNewLabel_2 = new JLabel("BATTAGLIA NAVALE ");
         lblNewLabel_2.setForeground(new Color(0, 128, 255));
-        lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 42));
+        lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 60));
         lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-        lblNewLabel_2.setBounds(129, 0, 499, 85);
+        lblNewLabel_2.setBounds(400, 0, 650, 85);
         backgroundPanel.add(lblNewLabel_2);
        
         final ImageIcon hoverIcon = new ImageIcon("../docs/resources/EffettoBottone.png");
 	    Image buttonImage = hoverIcon.getImage();
-        Image scaledButtonImage = buttonImage.getScaledInstance(70, 70, Image.SCALE_SMOOTH);
+        Image scaledButtonImage = buttonImage.getScaledInstance(90, 90, Image.SCALE_SMOOTH);
 	    final JLabel imageLabel =new JLabel(new ImageIcon(scaledButtonImage));
 	    final JLabel imageLabel2 =new JLabel(new ImageIcon(scaledButtonImage));
 	    final JLabel imageLabel3 =new JLabel(new ImageIcon(scaledButtonImage));
-	    imageLabel.setBounds(258,124,72,70);
-	    imageLabel2.setBounds(258,247,72,70);
-	    imageLabel3.setBounds(258,379,72,70);
+	    imageLabel.setBounds(545,154,90,90);
+	    imageLabel2.setBounds(545,315,90,90);
+	    imageLabel3.setBounds(545,480,90,90);
 	    imageLabel.setVisible(false);
 	    imageLabel2.setVisible(false);
 	    imageLabel3.setVisible(false);
@@ -106,10 +124,10 @@ public class MenuPrincipaleView extends JFrame {
 	    
 
 	    final JButton multiplayerButton = new JButton("");
-	    multiplayerButton.setPreferredSize(new Dimension(170, 50));
+	    multiplayerButton.setPreferredSize(new Dimension(370, 150));
 	    multiplayerButton.setBackground(new Color(0, 0, 0, 0));
 		multiplayerButton.setOpaque(false);
-	    multiplayerButton.setBounds(258, 124, 275, 70);
+	    multiplayerButton.setBounds(550, 154, 350, 90);//275 70
 	    backgroundPanel.add(multiplayerButton);
 	    
 	    multiplayerButton.addActionListener(new MenuPrincipaleControl(this));
@@ -121,8 +139,8 @@ public class MenuPrincipaleView extends JFrame {
 	    	{
 	    		imageLabel.setVisible(true);
 	    		multiplayerTextLabel.setForeground(new Color(0, 128, 255));
-	    		multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
-	    		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+	    		multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
+	    		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 17));
 	    		multiplayerButton.repaint();
 	    	}
 	    
@@ -131,8 +149,8 @@ public class MenuPrincipaleView extends JFrame {
 	    {
 	    	imageLabel.setVisible(false);
 	    	multiplayerTextLabel.setForeground(new Color(0, 0, 0));
-	    	multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-	    	lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 10));
+	    	multiplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 34));
+	    	lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 15));
             multiplayerButton.repaint();
              
 	    }
@@ -162,8 +180,8 @@ public class MenuPrincipaleView extends JFrame {
 	    
 	    final JLabel singleplayerTextLabel= new JLabel("Singleplayer");
         singleplayerTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-        singleplayerTextLabel.setBounds(324,266,155,30);
+        singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 34));
+        singleplayerTextLabel.setBounds(600,320,282,70);
         backgroundPanel.add(singleplayerTextLabel);
         
 	    final JButton singleplayer = new JButton("");
@@ -171,7 +189,7 @@ public class MenuPrincipaleView extends JFrame {
 	    singleplayer.setBackground(new Color(0, 0, 0, 0));
 		singleplayer.setOpaque(false);
 	    singleplayer.setPreferredSize(new Dimension(170, 50));
-	    singleplayer.setBounds(258, 247, 275, 70);
+	    singleplayer.setBounds(550, 317, 350, 90);
 	    backgroundPanel.add(singleplayer);
 	    
 	    
@@ -183,8 +201,8 @@ public class MenuPrincipaleView extends JFrame {
 	    	{
 	    		imageLabel2.setVisible(true);
 	    		singleplayerTextLabel.setForeground(new Color(0, 128, 255));
-	    		singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
-	    		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
+	    		singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
+	    		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 17));
 	    		singleplayer.repaint();
 	    	}
 	    
@@ -193,8 +211,8 @@ public class MenuPrincipaleView extends JFrame {
 	    {
 	    	imageLabel2.setVisible(false);
 	    	singleplayerTextLabel.setForeground(new Color(0, 0, 0));
-	    	singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-	    	lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 10));
+	    	singleplayerTextLabel.setFont(new Font("Tahoma", Font.BOLD, 34));
+	    	lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 15));
             singleplayer.repaint();
              
 	    }
@@ -226,15 +244,15 @@ public class MenuPrincipaleView extends JFrame {
 	    
 	    final JLabel tutorialTextLabel= new JLabel("Tutorial");
         tutorialTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-        tutorialTextLabel.setBounds(324,395,155,30);
+        tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 34));
+        tutorialTextLabel.setBounds(630,495,200,50);
         backgroundPanel.add(tutorialTextLabel);
 	    
 	    final JButton tutorialButton = new JButton("");
 	    tutorialButton.setBackground(new Color(0, 0, 0, 0));
 		tutorialButton.setOpaque(false);
 	    //tutorialButton.setBorderPainted(false);
-	    tutorialButton.setBounds(258, 379, 275, 70);
+	    tutorialButton.setBounds(550, 480, 350, 90);
 	    tutorialButton.setPreferredSize(new Dimension(170, 50)); // Set the button size in pixels
 	    backgroundPanel.add(tutorialButton);
 	    
@@ -244,8 +262,8 @@ public class MenuPrincipaleView extends JFrame {
 	    	{
 	    		imageLabel3.setVisible(true);
 	    		tutorialTextLabel.setForeground(new Color(0, 128, 255));
-	    		tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
-	    		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 12));
+	    		tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
+	    		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 17));
 	    		tutorialButton.repaint();
 	    	}
 	    
@@ -254,8 +272,8 @@ public class MenuPrincipaleView extends JFrame {
 	    {
 	    	imageLabel3.setVisible(false);
 	    	tutorialTextLabel.setForeground(new Color(0, 0, 0));
-	    	tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-	    	lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 10));
+	    	tutorialTextLabel.setFont(new Font("Tahoma", Font.BOLD, 34));
+	    	lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 15));
             tutorialButton.repaint();
              
 	    }
@@ -283,8 +301,8 @@ public class MenuPrincipaleView extends JFrame {
 
 	    });
 	    
-	    JPanel radioPanel= new JPanel();
-	    radioPanel.setBounds(638,435,100,100);
+	    /*JPanel radioPanel= new JPanel();
+	    radioPanel.setBounds(800,435,100,100);
 	    radioPanel.setOpaque(false);
 	    
 	    ButtonGroup languageGroup = new ButtonGroup();
@@ -298,7 +316,7 @@ public class MenuPrincipaleView extends JFrame {
         engRadioButton.setOpaque(false);
         radioPanel.add(engRadioButton);
         
-        backgroundPanel.add(radioPanel);
+        backgroundPanel.add(radioPanel);*/
         
         JPanel panel = new JPanel();
         panel.setBounds(629, 26, 109, 30);
