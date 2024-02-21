450c34f91a811b23fad2808a55bd0777e3f6b878
buscst <145765823+buscst@users.noreply.github.com>
2024-02-21 10:22:15 +0100

    Testing Sound

--- BattagliaNavale/src/BattagliaNavaleProject/client/SoundEffect.java
+++ BattagliaNavale/src/BattagliaNavaleProject/client/SoundEffect.java
@@ -2,18 +2,30 @@ package BattagliaNavaleProject.client;
 
 import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 public class SoundEffect 
 {
-	void playMusic(String posizioneFile)
+	public void playMusic(String posizioneFile)
 	{
 		try {
 			File pathMusica = new File(posizioneFile);
 			
 			if(pathMusica.exists())
 			{
-				AudioInputStream audioInput = AudioSystem.getAudioInputStream(null);
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(pathMusica);
+				Clip clip = AudioSystem.getClip(); //ottiene la stringa audio
+				clip.open(audioInput);
+				clip.start();
+				
+				JOptionPane.showMessageDialog(null, "Press OK to stop playing");
+			}
+			else
+			{
+				System.out.println("File musica non trovato");
 			}
 			
 		}catch(Exception e)
