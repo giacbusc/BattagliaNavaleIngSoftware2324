e63515df9a8b09529410a08c7e01f2ce8c95eadc
giessse <g.signori1@studenti.unibg.it>
2024-02-22 13:19:43 +0100

    prova dababase da server

---
+++ BattagliaNavaleServer/src/main/java/BattagliaNavaleProject/BattagliaNavaleServer/ProvaDb.java
@@ -0,0 +1,38 @@
+package BattagliaNavaleProject.BattagliaNavaleServer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProvaDb {
+
+    public static void main(String[] args) throws SQLException {
+        ConnectionDb conn1 = new ConnectionDb();
+        String sql = "SELECT * FROM utente";
+        PreparedStatement pstmt = conn1.getConnection().prepareStatement(sql);
+
+        ResultSet rs = pstmt.executeQuery();
+
+        // Itera attraverso le righe del risultato
+        while (rs.next()) {
+            // Ottieni i valori dalle colonne (supponendo colonne con nomi specifici)
+            String nickname = rs.getString("NICKNAME");
+            String nome = rs.getString("NOME");
+            String cognome = rs.getString("COGNOME");
+            String password = rs.getString("PASSWORD");
+
+            // Costruisci una stringa di output con i valori ottenuti
+            String resultString = String.format("Nickname: %s, Nome: %s, Cognome: %s, Password: %s",
+                    nickname, nome, cognome, password);
+
+            // Stampa o elabora la stringa di output come preferisci
+            System.out.println(resultString);
+        }
+        
+        // Chiudi il ResultSet e la connessione quando hai finito
+        rs.close();
+        pstmt.close();
+        conn1.closeConnection();
+    }
+}
+
