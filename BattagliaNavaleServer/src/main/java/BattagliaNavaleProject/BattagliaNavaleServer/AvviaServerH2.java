3ed5d3796b98b79b36a2ebd9ac431663a644d7f9
giessse <g.signori1@studenti.unibg.it>
2024-02-24 16:04:41 +0100

    prova server

---
+++ BattagliaNavaleServer/src/main/java/BattagliaNavaleProject/BattagliaNavaleServer/AvviaServerH2.java
@@ -0,0 +1,33 @@
+package BattagliaNavaleProject.BattagliaNavaleServer;
+import org.h2.tools.Server;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class AvviaServerH2 {
+	public static String serverAddress;
+    public static String avviaserver() {
+        try {
+            // Avvia il server H2 sulla porta specificata (es. 9092) e il percorso del database
+            String indirizzoIP = "192.168.1.4"; // Modifica con l'indirizzo IP appropriato
+            int porta = 5545; // Modifica con la porta appropriata
+
+            Server server = Server.createTcpServer("-tcpPort", Integer.toString(porta), "-tcpAllowOthers", "-ifNotExists");
+            server.start();
+
+            // Genera l'indirizzo del server per i client
+             serverAddress = "tcp://" + indirizzoIP + ":" + porta;
+           
+            System.out.println("Server H2 avviato. Puoi connetterti al database tramite: " + serverAddress);
+
+
+        } catch (Exception e) {
+            System.err.println("Errore durante l'avvio del server H2: " + e.getMessage());
+        }
+		return serverAddress;
+     }
+    
+    }
+
+
--- BattagliaNavaleServer/src/main/java/BattagliaNavaleProject/BattagliaNavaleServer/AvvioServer.java
+++ BattagliaNavaleServer/src/main/java/BattagliaNavaleProject/BattagliaNavaleServer/AvvioServer.java
@@ -6,7 +6,7 @@ public class AvvioServer
 	public static void main(String[] args) throws InterruptedException
 	{
 		ServerSocket server = ServerSocket.getInstance();
-		String tcp= "tcp://192.168.1.4:5554";
+		String tcp= AvviaServerH2.avviaserver();
 		String local="tcp://localhost:5545";
 		server.startServer(tcp);
 
