d2322fc2f6ca7a2f1819c50e064844047c56637b
buscst <145765823+giacbusc@users.noreply.github.com>
2024-03-14 16:24:04 +0100

    Aggiornato database con nuovi dati e sistemato registrazione

--- BattagliaNavale/src/BattagliaNavaleProject/control/RegistrationControl.java
+++ BattagliaNavale/src/BattagliaNavaleProject/control/RegistrationControl.java
@@ -14,42 +14,41 @@ import BattagliaNavaleProject.formModel.RegistrationModel;
 import BattagliaNavaleProject.view.RegistrationView;
 
 public class RegistrationControl implements ActionListener {
-	
+
 	private RegistrationModel model;
-    private RegistrationView view;
-    private LoginControl log;
-    
- 
-    public RegistrationControl( ){
-        view = new RegistrationView();
-        view.setVisible(true);
-        view.addRecListener(this);
-        view.addActionBack(this);
-    }
-    
-    //metodo che verifica se tutti i campi hanno al loro interno qualcosa
-    public boolean verificaCampi(RegistrationModel user){
-		if(user.getName().isEmpty() || user.getSurname().isEmpty() || user.getNickname().isEmpty() || user.getPassword().isEmpty()){
+	private RegistrationView view;
+	private LoginControl log;
+
+	public RegistrationControl() {
+		view = new RegistrationView();
+		view.setVisible(true);
+		view.addRecListener(this);
+		view.addActionBack(this);
+	}
+
+	// metodo che verifica se tutti i campi hanno al loro interno qualcosa
+	public boolean verificaCampi(RegistrationModel user) {
+		if (user.getName().isEmpty() || user.getSurname().isEmpty() || user.getNickname().isEmpty()
+				|| user.getPassword().isEmpty()) {
 			return false;
 		}
-		
+
 		return true;
 	}
-    
-    //metodo che verifica sei il nome e il cognome è composto da sole lettere
-    public boolean verificaNameSurname(RegistrationModel user){
-		if(!(( user.getName()).matches("[a-zA-Z]+") || !user.getSurname().matches("[a-zA-Z]+"))) {
+
+	// metodo che verifica sei il nome e il cognome è composto da sole lettere
+	public boolean verificaNameSurname(RegistrationModel user) {
+		if (!((user.getName()).matches("[a-zA-Z]+") || !user.getSurname().matches("[a-zA-Z]+"))) {
 			return false;
 		}
-		
+
 		return true;
 	}
-    
-    //metodo che verifica se il nickname è già presente all'interno del database
-    public boolean verificaNickname(RegistrationModel user) throws SQLException
-	{
+
+	// metodo che verifica se il nickname è già presente all'interno del database
+	public boolean verificaNickname(RegistrationModel user) throws SQLException {
 		ConnectionDb conn = new ConnectionDb();
-		
+
 		String nickname = user.getNickname();
 		System.out.println(user.getNickname());
 		String sql = "SELECT nickname FROM UTENTE WHERE nickname = ?";
@@ -57,72 +56,66 @@ public class RegistrationControl implements ActionListener {
 		pstmt.setString(1, nickname);
 
 		ResultSet rs = pstmt.executeQuery();
-		pstmt.close();
-        if (rs.next()) {
-       	 return false;
-         }
-         else 
- 		{
- 			return true;
- 		}
-		
-	}
-    
-    public void actionPerformed(ActionEvent e) {
-        
-        try {
-            
-            if(e.getSource() instanceof JButton ) {
-                JButton clickedButton= (JButton) e.getSource();
-                
-                //Se l'utente preme il pulsante Back viene chiamato il metodo close presente in questa classe
-                if(clickedButton.getText().equals("Back")) {
-                    close();
-                }
-                
-                //Se l'utente preme il pulsante Save viene verificata la correttezza dei dati
-                if(clickedButton.getText().equals("Save")) 
-                {   
-                    model = view.getUser();
-                    
-                    if( verificaCampi(model) && verificaNameSurname(model) && verificaNickname(model)) 
-                    {
-                        if(checkUser(model)) 
-                        {
-                        	//Se i dati sono corretti allora viene aperta la finestra di login
-                            view.showMessage("Registration complete!");
-                            openLogin();
-                            
-                        } 
-                        else 
-                        {
-                        	//Se i dati non sono corretti l'utente deve re-inserirli
-                            view.showMessage("Incorrect data entered, please re-enter it!");
-                        }     
-                    } 
-                    else 
-                    {
-                         view.showMessage("Incorrect data entered, please re-enter it");
-                    }
-                }
-                
-                
-            }
-        }
-        
-        catch (Exception ex) {
-            view.showMessage(ex.getMessage());
-            ex.printStackTrace(); 
-            
-        }
-    }
-        
 
-    //Metodo che inserisce all'interno del database i dati dell'utente che ha appena effettuato la registrazione
-    public boolean checkUser(RegistrationModel user) throws Exception {
- 
-    	String sql = "INSERT INTO UTENTE VALUES (?,?,?,?)";
-    	
+		if (rs.next()) 
+		{
+			pstmt.close();
+			return false;
+		} else {
+			pstmt.close();
+			return true;
+		}
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+
+		try {
+
+			if (e.getSource() instanceof JButton) {
+				JButton clickedButton = (JButton) e.getSource();
+
+				// Se l'utente preme il pulsante Back viene chiamato il metodo close presente in
+				// questa classe
+				if (clickedButton.getText().equals("Back")) {
+					close();
+				}
+
+				// Se l'utente preme il pulsante Save viene verificata la correttezza dei dati
+				if (clickedButton.getText().equals("Save")) {
+					model = view.getUser();
+
+					if (verificaCampi(model) && verificaNameSurname(model) && verificaNickname(model)) {
+						if (checkUser(model)) {
+							// Se i dati sono corretti allora viene aperta la finestra di login
+							view.showMessage("Registration complete!");
+							openLogin();
+
+						} else {
+							// Se i dati non sono corretti l'utente deve re-inserirli
+							view.showMessage("Incorrect data entered, please re-enter it!");
+						}
+					} else {
+						view.showMessage("Incorrect data entered, please re-enter it");
+					}
+				}
+
+			}
+		}
+
+		catch (Exception ex) {
+			view.showMessage(ex.getMessage());
+			ex.printStackTrace();
+
+		}
+	}
+
+	// Metodo che inserisce all'interno del database i dati dell'utente che ha
+	// appena effettuato la registrazione
+	public boolean checkUser(RegistrationModel user) throws Exception {
+
+		String sql = "INSERT INTO UTENTE VALUES (?,?,?,?)";
+
 		ConnectionDb conn = new ConnectionDb();
 		PreparedStatement pstmt = conn.getConnection().prepareStatement(sql);
 		pstmt.setString(1, user.getName());
@@ -133,29 +126,29 @@ public class RegistrationControl implements ActionListener {
 		pstmt.close();
 		return true;
 
-      }
+	}
 
 	public RegistrationView getView() {
 		// TODO Auto-generated method stub
 		return view;
 	}
-	
-	//Metodo utilizzato per aprire la finestra di Login
-	public void openLogin() {
-		  
-        log = new LoginControl();
-        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
-	        @Override
-	        protected Void doInBackground() throws Exception {
-	            //Chiudiamo qui la finestra di registrazione
-	        	view.dispose(); 
-	            return null;
-	        }
-	    };
 
-	    worker.execute(); 
+	// Metodo utilizzato per aprire la finestra di Login
+	public void openLogin() {
+
+		log = new LoginControl();
+		SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				// Chiudiamo qui la finestra di registrazione
+				view.dispose();
+				return null;
+			}
+		};
+
+		worker.execute();
 	}
-	
+
 	public void close() {
 		view.dispose();
 		view.getObserver().update();
